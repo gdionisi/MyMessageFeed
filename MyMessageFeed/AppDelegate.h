aec2106c81c624d5db83a9f8d8e7e735b431a5de
gdionisi <gdionisi@lbl.gov>
2017-05-31 15:52:27 -0700

    Initial Commit

---
+++ MyMessageFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyMessageFeed
+//
+//  Created by Guillaume Dionisi on 5/31/17.
+//  Copyright © 2017 MyGreenCar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyMessageFeed/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyMessageFeed
+//
+//  Created by Guillaume Dionisi on 5/31/17.
+//  Copyright © 2017 MyGreenCar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
